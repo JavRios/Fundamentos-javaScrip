93571852189d2ef7d412c47da733071f11375c91
Francisco Javier Rios Cruz <rios_879@hotmail.com>
2020-07-12 14:22:27 -0500

    manejo de cadenas y templetes de cadena

--- clase1.js
+++ clase1.js
@@ -1,1 +1,8 @@
-console.log ("Hola Mundo")
+var nombre = 'Francisco', apellido = 'Rios'
+
+
+var nombreEnMayusculas = nombre.toUpperCase () // para comvertir a mayusculas
+var apelledioEnMinusculas = apellido.toLowerCase () //para comvertir a minusculas
+var primeraLetra = apellido.charAt(1)
+var largoCadena = nombre.length
+var nombreCompleto = `${nombre} ${apellido.toUpperCase}`// con esta cadena templete puedes escribir codigo dentro de ellos

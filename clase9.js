69e5c68bacf6cbc0119835c02150abb0d9b6077e
Francisco Javier Rios Cruz <rios_879@hotmail.com>
2020-07-13 00:59:40 -0500

    nada nuevo

---
+++ clase9.js
@@ -0,0 +1,11 @@
+var x = 4, y = '4'
+
+var sacha = {
+  nombre: 'Sacha'
+}
+
+var otraPersona = {
+  nombre: 'Sacha'
+}
+
+//un objeto literal no se puede comparar con totro al menos que esta apunto al mismo objeto en la misma posicion de la memoria, es decir, como si fuese un clon

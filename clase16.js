384e227d15eab1891c2eff6fb6133262bb7d39b6
Francisco Javier Rios Cruz <rios_879@hotmail.com>
2020-07-18 18:21:34 -0500

    ciclo while sigon sin saber bien para que sirve

---
+++ clase16.js
@@ -0,0 +1,14 @@
+var signo   = prompt ('¿Cual es tu signo zodiacal')
+
+switch (signo) {
+    case 'acuario':
+        console.log ('eres acuario')
+        break;
+    case 'geminis':
+        console.log ('Eres Genial')
+        break;
+
+    default:
+        console.log('falta terminar el switch')
+        break;
+}
